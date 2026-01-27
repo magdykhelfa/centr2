b4194d410d55f2647c248f8224dbe784e875ec71
Magdy Khelfa <magdy.khelfa.eg@gmail.com>
2026-01-27 16:50:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/centr/',
+  base: '/centr2/', // لازم يكون نفس اسم الريبو بالظبط
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
