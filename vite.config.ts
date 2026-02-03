cdb2a8bf8f88196036183aec76bf4d79cd95a8b3
Magdy Khelfa <magdy.khelfa.eg@gmail.com>
2026-02-03 19:06:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite' // السطر ده هو اللي ناقص!
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url' // سطر إضافي للأمان
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   base: '/centr2/', 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
